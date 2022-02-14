c03837ef2a0960b9a08273321d7a0c603bd5b507
emilyk <emily.g.kruse@gmail.com>
2022-02-14 13:24:19 -0500

    communicate with player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running!");
+// define playerName from user input
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+// This will log the robot's name
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
